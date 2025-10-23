812c9a10db06b4eff27ecba5ee93e57de1cccaf5
Murari Singh <murarisingh847407@gmail.com>
2025-10-23 21:40:03 +0530

    made fully functional calculator app using ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,69 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [curVal, setCurlVal] = useState("");
+  const [prevVal, setPrevVal] = useState("");
+  const [op, setOp] = useState("");
 
+  function handleDecimalClick(i) {
+    setCurlVal((prev) => prev + i);
+  }
+  function handleClear() {
+    setCurlVal("");
+    setPrevVal("");
+    setOp("");
+  }
 
-const [input, setInput] = useState("");
+  function handlePercent() {
+    setCurlVal((prev) => prev / 100);
+  }
+  function handleSqrt() {
+    setCurlVal((prev) => Math.sqrt(prev));
+  }
+  function handleOp(op) {
+    setOp(op);
+    setPrevVal(curVal);
+    setCurlVal("");
+  }
+  function handleEquals() {
+    const prev = parseFloat(prevVal);
+    const curr = parseFloat(curVal);
+
+    if (op === "÷") {
+      setCurlVal(prev / curr);
+      setPrevVal("");
+      setOp("");
+    } else if (op === "x") {
+      setCurlVal(prev * curr);
+      setPrevVal("");
+      setOp("");
+    } else if (op === "-") {
+      setCurlVal(prev - curr);
+      setPrevVal("");
+      setOp("");
+    } else if (op === "+") {
+      setCurlVal(prev + curr);
+      setPrevVal("");
+      setOp("");
+    }
+  }
+  const handleDisplay = () => {
+    if (curVal.length === 0) {
+      return 0;
+    }  else if (curVal === '.') {
+      return "0.";
+    }
+    
+    else {
+      return parseFloat(curVal).toLocaleString();
+    }
+  };
 
   return (
     <>
-    <div className="h-screen w-screen  bg-gray-100">
-
-
-      <div
-        className="max-w-[400px] grid grid-cols-4 gap-6 font-bold text-2xl
+      <div className="h-screen w-screen  bg-gray-100">
+        <div
+          className="max-w-[400px] grid grid-cols-4 gap-6 font-bold text-2xl
                     [&>button]:bg-red-400 
                       [&>button]:rounded-xl 
                       [&>button]:py-3 
@@ -22,41 +74,46 @@ const [input, setInput] = useState("");
                       [&>button]:active:scale-95"
         >
           {/* Main screen result showing */}
-          <div className="col-span-4 gap-y-10">it will show result here</div>
-          
+          <div className="col-span-4 gap-y-10 text-right">{handleDisplay()}</div>
+
           {/* first row of calculator */}
 
-          <button>(</button>
-          <button>C</button>
-          <button>)</button>
-          <button>/</button>
+          <button onClick={() => handleClear()}>C</button>
+          <button onClick={() => handlePercent()}>%</button>
+          <button onClick={() => handleSqrt()}>√</button>
+          <button onClick={() => handleOp("÷")}>÷</button>
 
           {/* Second Row */}
 
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>x</button>
+          <button onClick={() => handleDecimalClick("7")}>7</button>
+          <button onClick={() => handleDecimalClick("8")}>8</button>
+          <button onClick={() => handleDecimalClick("9")}>9</button>
+          <button onClick={() => handleOp("x")}>x</button>
 
           {/* Third Row */}
 
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button onClick={() => handleDecimalClick("4")}>4</button>
+          <button onClick={() => handleDecimalClick("5")}>5</button>
+          <button onClick={() => handleDecimalClick("6")}>6</button>
+          <button onClick={() => handleOp("-")}>-</button>
 
           {/* Fourth Row */}
 
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
+          <button onClick={() => handleDecimalClick("1")}>1</button>
+          <button onClick={() => handleDecimalClick("2")}>2</button>
+          <button onClick={() => handleDecimalClick("3")}>3</button>
+          <button onClick={() => handleOp("+")}>+</button>
 
           {/* fifth row */}
 
-          <button className="col-span-2">0</button>
-          <button>.</button>
-          <button>=</button>
+          <button
+            className="col-span-2"
+            onClick={() => handleDecimalClick("0")}
+          >
+            0
+          </button>
+          <button onClick={() => handleDecimalClick(".")}>.</button>
+          <button onClick={() => handleEquals()}> =</button>
         </div>
       </div>
     </>
